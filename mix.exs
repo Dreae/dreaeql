9ce15c103679d8b8fcccc967f9d0d15b1ae0385e
dreae <dreae@dreae.onl>
2020-08-22 14:45:38 -0400

    Add hex details, license

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Dreaeql.MixProject do
+defmodule DreaeQL.MixProject do
   use Mix.Project
 
   def project do
@@ -7,7 +7,9 @@ defmodule Dreaeql.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
     ]
   end
 
@@ -18,11 +20,21 @@ defmodule Dreaeql.MixProject do
     ]
   end
 
+  defp description do
+    "A library for parsing a simple query language into an abstract syntax tree."
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Dreae/dreaeql"}
     ]
   end
 end
